252f1a0a5606f86ec2357b9d08f9d8e78be23df5
Ekaterina Boyarscih <72408556+EkaterinaBoyarscih39@users.noreply.github.com>
2021-04-15 01:36:22 +0500

    Update Cars.java

--- Cars.java
+++ Cars.java
@@ -1,24 +1,19 @@
 package com.cars;
 
 public class Cars {
-    public String code_cars;//Код транспортного средства
+    public String code_car;//Код транспортного средства
     public String reg_numbers;//Гос.номер
     public int car_mileage;//Пробег
-    public String add_param;//Доп.параметр
+    public int add_param;//Доп.параметр
     public double cost_fuel;//Стоимость литра топлив
     public double fuel;//Расход топлива
-
-    public Cars(int car_mileage, double cost_fuel, double fuel) {
+    public String type_cars;//Тип авто
+    public Cars(String code_car, String reg_numbers, int car_mileage, int add_param) {
+        this.code_car = code_car;
+        this.reg_numbers = reg_numbers;
         this.car_mileage = car_mileage;
-        this.cost_fuel = cost_fuel;
-        this.fuel = fuel;
+        this.add_param = add_param;
     }
 
-    public Cars(double cost_fuel, double fuel) {
-    }
-
-    public double full_cost() {
-        //Затрачено на ГСМ за всё время использования
-        return car_mileage/100 * cost_fuel * fuel;
-    }
 }
+
